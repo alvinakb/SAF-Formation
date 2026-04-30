b2cfa8c53fb8397012cd34ecc053838c08c1024d
alvinakb <alvinak@outlook.fr>
2026-04-30 22:16:38 +0000

    nos-valeurs.spec.ts

--- src/app/pages/nos-valeurs/nos-valeurs.spec.ts
+++ src/app/pages/nos-valeurs/nos-valeurs.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NosValeurs } from './nos-valeurs';
+import { Nosvaleurs } from './nos-valeurs';
 
 describe('NosValeurs', () => {
   let component: NosValeurs;
